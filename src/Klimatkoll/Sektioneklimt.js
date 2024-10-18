bf0f317defbb55ce3ad02aa97717620e38e687bf
Marohaa <s2105409@student.hb.se>
2024-10-18 10:40:33 +0100

    Fixa styling

--- src/Klimatkoll/Sektioneklimt.js
+++ src/Klimatkoll/Sektioneklimt.js
@@ -28,7 +28,7 @@ const Sektionklimt = () => {
             <p><strong>Project Type:</strong> Design an educating website</p>
             <p><strong>Role:</strong> User research + UX/UI designer + brand designer + implementation</p>
             <p><strong>Industry:</strong> Educational, Climate</p>
-            <p><strong>Tools:</strong> Figma, Canva, Illustrator, ReactJS</p>
+            <p><strong>Tools:</strong> Figma, Canva, Illustrator, ReactJS, Firebase</p>
             <p><strong>Duration:</strong> 2 months, 2024</p>
             <Button 
               href="https://www.figma.com/proto/cZxBC4r3YraBsDtpqB4IjC/Prototyp?node-id=55-3&node-type=canvas&t=Sok9u0fcFfz6DrjO-0&scaling=scale-down-width&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=34%3A2" 
@@ -55,7 +55,7 @@ const Sektionklimt = () => {
            
             <li>Personas</li>
             <li>Scenario</li>
-            <li>User jouneys</li>
+          
             
           </ul>
         </Col>
@@ -64,9 +64,8 @@ const Sektionklimt = () => {
         <Col xs={12} md={4} className=" text-center"> {/* Lägg till text-center för att centrera texten */}
           <h3 className="fancy-underline" style={{ fontFamily: 'Patrick Hand, cursive' }}>Define</h3>
           <ul style={{ listStyleType: 'none', padding: '20px' }}>
-            <li>Funktioner samt krav</li>
+            <li>MoSCoW </li>
             <li>UML-diagram</li>
-            <li>User stories</li>
           </ul>
         </Col>
 
@@ -74,12 +73,12 @@ const Sektionklimt = () => {
         <Col xs={12} md={4} className=" text-center"> {/* Lägg till text-center för att centrera texten */}
           <h3 className="fancy-underline" style={{ fontFamily: 'Patrick Hand, cursive' }}>Develop</h3>
           <ul style={{ listStyleType: 'none', padding: '20px'}}>
-            <li>Lo-fi prototyp</li>
-            <li>Designsystem</li>
+            <li>Lo-fi prototype            </li>
+            <li>Design system</li>
             <li>Wireframes</li>
             <li>Prototyping</li>
             <li>Implementation</li>
-            <li>Bra och kunde gjort annorlunda</li>
+            <li>Pros and cons</li>
           </ul>
         </Col>
       </Row>
@@ -115,6 +114,8 @@ const Sektionklimt = () => {
         <li><strong>Engage Diverse Audiences:</strong> To provide an inclusive platform that caters to students aged 13-15 from various backgrounds, regardless of their prior knowledge.</li>
         <li><strong>Interactive Learning:</strong> To incorporate various educational tools, including quizzes and challanges, to enhance understanding and foster sustainable behaviors.</li>
       </ul>
+      <p className='p-4'></p>
+
       <h2 style={{ fontFamily: 'Patrick Hand, cursive' }}>Target Audience Research</h2>
       
       <p>The target audience for this website is teenagers aged 13-15 in Swedish schools, as defined by the educators.
@@ -137,21 +138,21 @@ const Sektionklimt = () => {
   <Row className="justify-content-center pb-5">
     <Col md={8} className=' text-center' >
       {/* First paragraph centered */}
-      <p className="lead fancy-underline text-center">Personas</p>
+      <p className="lead fancy-underline text-center p-2">Personas</p>
       {/* Second paragraph left-aligned but horizontally centered */}
       <p className="text-left">
         Two personas, Alicia Perry and Gabriel Andersson, were created based on the collected data to represent different segments of the target audience. Alicia, our primary persona, reflects the more engaged segment interested in climate issues, while Gabriel represents those with less knowledge or interest in the topic. 
       </p>
     </Col>
 <p className='p4'></p>
-    <Col xs={12} md={5} className="d-flex justify-content-center pb-3 shadow">
+    <Col xs={12} md={6} className="d-flex justify-content-center p-3">
       <img 
         src="/Photos/persona 1.png" 
         alt="Persona Alicia" 
         className="img-fluid"
       />
     </Col>
-    <Col xs={12} md={5} className="d-flex justify-content-center pb-3 shadow">
+    <Col xs={12} md={6} className="d-flex justify-content-center p-3 ">
       <img 
         src="/Photos/persona 2.png" 
         alt="Persona Gabriel" 
@@ -161,125 +162,100 @@ const Sektionklimt = () => {
   </Row>
 </Container>
 
+<Container fluid className="d-flex justify-content-center align-items-center p-4" style={{backgroundColor:'#f8f9fa'}}>
+  <Row className="justify-content-center pb-5">
+    <Col md={8} className=' text-center' >
+      {/* First paragraph centered */}
+      <p className="lead fancy-underline text-center p-2">Scenario</p>
+      {/* Second paragraph left-aligned but horizontally centered */}
+      <p className="text-left">
+      Detailed scenarios for each persona were crafted to better represent their expectations and create a more engaging experience.
+      </p>
+    </Col>
+<p className='p4'></p>
+    <Col xs={12} md={6} className="d-flex justify-content-center p-3">
+      <img 
+        src="/Photos/AlicaSen.png" 
+        alt="Persona Alicia" 
+        className="img-fluid"
+      />
+    </Col>
+    <Col xs={12} md={6} className="d-flex justify-content-center p-3 ">
+      <img 
+        src="/Photos/gaberilsSen.png" 
+        alt="Persona Gabriel" 
+        className="img-fluid"
+      />
+    </Col>
+  </Row>
+</Container>
 
+    
 
- 
-    <div className="justify-content-center text-center"> 
-    <h2 style={{ backgroundColor: '#E46698', color: 'white', padding: '30px', fontFamily: 'Patrick Hand, cursive' }}>
-    Define phase
-          </h2>
-    </div>
-    <div style={{paddingTop:'20px', paddingBottom:'20px'}}></div>
-    <div className="d-flex justify-content-center mt-4">
-  <img 
-    src="/Photos/Emma (1).png"  // Replace with your image URL
-    alt="persona"
-    className="img-fluid shadow"  // This class makes the image responsive
-    style={{ maxWidth: '800px', width: '100%', height: 'auto' }} // Max width 500px, with responsive behavior
-  />
-</div>
 <div style={{paddingTop:'20px', paddingBottom:'20px'}}></div>
 
-    <div className="justify-content-center text-center"> 
-    <h2 style={{ backgroundColor: '#F5C518', color: 'black', padding: '30px', fontFamily: 'Patrick Hand, cursive'}}>
-    MVH & User flow
+    <div className="justify-content-center text-center" style={{backgroundColor:'white'}}> 
+    <h2 style={{ backgroundColor: '#0B6BA5', color: 'White', padding: '30px', fontFamily: 'Patrick Hand, cursive'}}>
+    Define phase
           </h2>
-          <p className="lead fancy-underline">Define phase</p>
+          <p className="lead fancy-underline text-center p-2">MoSCoW</p>
     </div>
     <section className="container py-5">
-  <h2 className="text-center mb-4" style={{ fontFamily: 'Patrick Hand, cursive' }}>HMWs to Product Goals</h2>
+  <p className="text-center mb-4">The MoSCoW method is a prioritization technique used to manage requirements in a project</p>
   <div className="row justify-content-center">
-    <div className="col-md-4 mb-4">
-      <div className="p-4 shadow" style={{ backgroundColor: '#e46698', borderRadius: '8px', color: 'white' }}>
-        <h5>How might we help the user keep their wishlist items after logging in?</h5>
-        <p>By implementing persistent storage mechanisms such as local storage or syncing wishlist data with the user's account, we ensure the wishlist is retained after login.</p>
+    <div className="col-md-4 mb-4 d-flex">
+      <div className="p-4 shadow flex-grow-1" style={{ backgroundColor: '#e46698', borderRadius: '8px', color: 'white' }}>
+        <h5 style={{fontFamily: 'Patrick Hand, cursive'}}>Key Requirements for Alicia</h5>
+        <ul>
+          <li>Must have: Easy navigation, clear graphs, minimal text.</li>
+          <li>Should have: Videos, quizzes, challenges, profile picture options.</li>
+          <li>Could have: Log-in page.</li>
+          <li>Want to have: A page where different schools can compete against each other.</li>
+        </ul>
       </div>
     </div>
-    <div className="col-md-4 mb-4">
-      <div className="p-4 shadow" style={{ backgroundColor: '#0b6ba5', borderRadius: '8px',  color: 'white' }}>
-        <h5>How might we help the user remember their wishlist items during the checkout process?</h5>
-        <p>Integrating wishlist reminders within the checkout flow, perhaps by displaying them in the cart, can help users remember and consider adding these items to their purchase.</p>
+    <div className="col-md-4 mb-4 d-flex">
+      <div className="p-4 shadow flex-grow-1" style={{ backgroundColor: '#0b6ba5', borderRadius: '8px',  color: 'white' }}>
+        <h5 style={{fontFamily: 'Patrick Hand, cursive'}}>Key Requirements for Gabriel</h5>
+        <ul>
+          <li>Must have: Basic information, three educational sections.</li>
+          <li>Should have: Quizzes, challenges, images.</li>
+          <li>Could have: Modern design, profile page with picture and collected points.</li>
+          <li>Want to have: Ability to compare scores with others in the app.</li>
+        </ul>
       </div>
     </div>
   </div>
 </section>
 
-    <h2 className="text-center mb-4" style={{ fontFamily: 'Patrick Hand, cursive' }}>User flow</h2>
+
+
     
     <div className="d-flex justify-content-center mt-5">
-        
-      <img 
-        src="/Photos/Green and White Project Phases Flowchart.png" // Replace with your image URL
+    <section className="container py-5">
+  <div className="row justify-content-center">
+    <div className="col-md-12 text-center">
+    <p className="lead fancy-underline text-center p-2">UML-diagram</p>
+    </div>
+    <div className="col-md-8 text-center">
+    <img 
+        src="/Photos/Uml.png" // Replace with your image URL
         alt="persona"
         className="img-fluid shadow" // This class makes the image responsive
         style={{ maxWidth: '800px', width: '100%', height: 'auto' }} // Optional for additional responsiveness
-      />
+      />    </div>
+  </div>
+</section>
+ 
+     
     </div>
     <div style={{paddingTop:'20px', paddingBottom:'20px'}}></div>
     <div className="justify-content-center text-center"> 
-    <h2 style={{ backgroundColor: '#0b6ba5', color: 'white', padding: '30px', fontFamily: 'Patrick Hand, cursive'  }}>
-    Wireframes
+    <h2 style={{ backgroundColor: '#F5C518', color: 'black', padding: '30px', fontFamily: 'Patrick Hand, cursive'  }}>
+    Develop phase
           </h2>
     </div>
-    <div style={{paddingTop:'20px', paddingBottom:'20px'}}></div>
-    <div id="imageCarousel" className="carousel slide" data-bs-ride="carousel">
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img src="/Photos/Namnlös design (46).png" className="d-block w-100 img-fluid shadow" alt="First slide" style={{ maxWidth: '800px', margin: '0 auto' }} />
-        </div>
-        <div className="carousel-item">
-          <img src="/Photos/Wifi.png" className="d-block w-100 img-fluid shadow" alt="Second slide" style={{ maxWidth: '800px', margin: '0 auto' }} />
-        </div>
-        <div className="carousel-item">
-          <img src="/Photos/Wifi (1).png" className="d-block w-100 img-fluid shadow" alt="Third slide" style={{ maxWidth: '800px', margin: '0 auto' }} />
-        </div>
-      </div>
-      <button className="carousel-control-prev" type="button" data-bs-target="#imageCarousel" data-bs-slide="prev">
-  <span className="carousel-control-prev-icon" aria-hidden="true">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="black" className="bi bi-arrow-left" viewBox="0 0 16 16">
-      <path fillRule="evenodd" d="M15 8a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 0 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 0 1 .708.708L2.707 7.5H14.5A.5.5 0 0 1 15 8z"/>
-    </svg>
-  </span>
-  <span className="visually-hidden">Previous</span>
-</button>
-
-<button className="carousel-control-next" type="button" data-bs-target="#imageCarousel" data-bs-slide="next">
-  <span className="carousel-control-next-icon" aria-hidden="true">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="black" className="bi bi-arrow-right" viewBox="0 0 16 16">
-      <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 1 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-    </svg>
-  </span>
-  <span className="visually-hidden">Next</span>
-</button>
-</div>
-<div style={{paddingTop:'20px', paddingBottom:'20px'}}></div>
-<div className="justify-content-center text-center mt-4"> 
-    <h2 style={{ backgroundColor: '#0b6ba5', color: 'white', padding: '30px', fontFamily: 'Patrick Hand, cursive' }}>
-    Prototyp
-          </h2>
-
-    </div>
     
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', margin:'20px'}}>
-    
-  <Button 
-    href="https://www.figma.com/proto/pOWFMhv3VEgEDve2R6JBAN/Marah?node-id=1-3&node-type=canvas&t=tpaCGDBPEb2loNS1-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A3" 
-    style={{ backgroundColor: '#F5C518', color: 'black', border: 'none', padding: '12px 24px', borderRadius: '50px' }}
-  >
-    View hi-fi prototype
-  </Button>
-</div>
-    <div className="d-flex justify-content-center mt-5">
-        
-        <img 
-          src="/Photos/Wifi (2).png" 
-          alt="persona"
-          className="img-fluid shadow " // This class makes the image responsive
-          style={{ maxWidth: '100%', height: 'auto' }} // Optional for additional responsiveness
-        />
-      </div>
-      <div style={{paddingTop:'50px', paddingBottom:'50px'}}></div>
-      
     
     </>
   );
